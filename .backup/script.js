a9c967a1ce54ba2ef9f93d817b2db85805b35118
sunaipa <sunaipa5@users.noreply.github.com>
2023-11-05 22:53:28 +0300

    1.0
    Yea Simpss 1.0 done!!

--- .backup/script.js
+++ .backup/script.js
@@ -1,17 +1,19 @@
 function toggleTheme() {
-    const body = document.getElementById("body");
+    const body = document.getElementsByTagName("body")[0];
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
         body.classList.add("light");
         document.getElementById("dark").style.display = "block";
         document.getElementById("light").style.display = "none";
         localStorage.setItem("theme", "light");
+        
     } else if (body.classList.contains("light")) {
         body.classList.remove("light");
         body.classList.add("dark");
         localStorage.setItem("theme", "dark");
         document.getElementById("dark").style.display = "none";
         document.getElementById("light").style.display = "block";
+       
     } else {
         body.classList.remove("dark", "light");
         body.classList.add("light");
@@ -33,3 +35,21 @@ window.onload = function() {
         document.getElementById("light").style.display = "none";
     }
 };
+
+function ctheme(theme){
+const body = document.getElementsByTagName("body")[0];
+if(theme == "dark"){
+    body.classList.remove("light");
+    body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    document.getElementById("dark").style.display = "none";
+    document.getElementById("light").style.display = "block";
+}else if(theme == "light"){
+    body.classList.remove("dark");
+    body.classList.add("light");
+    localStorage.setItem("theme", "light");
+    document.getElementById("dark").style.display = "block";
+    document.getElementById("light").style.display = "none";
+}
+}
+
