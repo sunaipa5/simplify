3e084eeb6dbfdc88640893e81c0418151235b0dc
sunaipa <sunaipa5@users.noreply.github.com>
2023-11-06 13:41:09 +0300

    1.0
    toggleTheme.js hot fix

--- .backup/script.js
+++ .backup/script.js
@@ -6,14 +6,12 @@ function toggleTheme() {
         document.getElementById("dark").style.display = "block";
         document.getElementById("light").style.display = "none";
         localStorage.setItem("theme", "light");
-        
     } else if (body.classList.contains("light")) {
         body.classList.remove("light");
         body.classList.add("dark");
         localStorage.setItem("theme", "dark");
         document.getElementById("dark").style.display = "none";
         document.getElementById("light").style.display = "block";
-       
     } else {
         body.classList.remove("dark", "light");
         body.classList.add("light");
@@ -23,14 +21,15 @@ function toggleTheme() {
 
 window.onload = function() {
     const savedTheme = localStorage.getItem("theme");
+    const body = document.getElementsByTagName("body")[0];
     if (savedTheme === "dark") {
-        document.getElementById("body").classList.add("dark");
-        document.getElementById("body").classList.remove("light");
+        body.classList.add("dark");
+        body.classList.remove("light");
         document.getElementById("dark").style.display = "none";
         document.getElementById("light").style.display = "block";
     } else {
-        document.getElementById("body").classList.add("light");
-        document.getElementById("body").classList.remove("dark");
+        body.classList.add("light");
+        body.classList.remove("dark");
         document.getElementById("dark").style.display = "block";
         document.getElementById("light").style.display = "none";
     }
